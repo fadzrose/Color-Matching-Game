a6b669e49c75057e518e7148eb67824a6bb15e99
irdinafadzli <irdinafadzli13@gmail.com>
2025-01-29 22:06:35 +0800

    ojojo

--- app/src/main/java/com/example/colormatchinggame/SplashActivity.java
+++ app/src/main/java/com/example/colormatchinggame/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             finish();
         }, SPLASH_DELAY);
     }
